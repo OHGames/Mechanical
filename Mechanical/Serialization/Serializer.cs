275c0f8dc596746903fdaa0710504cbcc95ac1dd
O.H. Games <88989246+OHGames@users.noreply.github.com>
2021-11-24 14:56:59 -0800

    ⚒WIP Serializer ⚒
    Making a light "wrapper" for the DataContractSerializer

---
+++ Mechanical/Serialization/Serializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Mechanical.Serialization
+{
+    /// <summary>
+    /// This class is a wrapper for the <see cref="DataContractSerializer"/>. (because its funky!!!) >:O
+    /// </summary>
+    public static class Serializer
+    {
+
+        /// <summary>
+        /// A list of types that need to be serialized.
+        /// </summary>
+        public static List<Type> KnownTypes = new List<Type>();
+
+        /// <summary>
+        /// The actual serializer.
+        /// </summary>
+        public static DataContractSerializer DataContractSerializer;
+
+        public void Serialize<T>(T obj, DataContractSerializerSettings settings = null)
+        {
+            Type t = typeof(T);
+            if (settings != null)
+            {
+                DataContractSerializer = new DataContractSerializer(t, settings);
+            }
+            else
+            {
+                DataContractSerializer = new DataContractSerializer(t, KnownTypes);
+            }
+        }
+
+    }
+}
