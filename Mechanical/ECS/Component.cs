cd70d2aa825902a1b1827bd5fbfcf8a3dd72760b
O.H. Games <88989246+OHGames@users.noreply.github.com>
2021-11-27 18:03:21 -0800

    ❗Added license info to all files ❗
    May remove in the future.

--- Mechanical/ECS/Component.cs
+++ Mechanical/ECS/Component.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.Xna.Framework.Content;
+﻿/*
+ * 
+ * This file is part of the Mechanical Game Engine and is licensed under the MIT license.
+ * https://github.com/OHGames/Mechanical
+ * 
+ * By O.H. Games
+ * 
+ * Note: some files contain code from other sources so see https://github.com/OHGames/Mechanical/blob/main/USED_CODE_LICENSES.txt for more info.
+ */
+
+using Microsoft.Xna.Framework.Content;
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
