8218a150bcb05c1c945bf597d7d8ef8dd2d451bc
O. H. Games <owen@oh-games.com>
2022-03-19 13:15:14 -0700

    Started development of an aseprite importer.

---
+++ Mechanical.Aseprite/AsepriteFile.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * 
+ * This file is part of the Mechanical Game Engine and is licensed under the MIT license.
+ * https://github.com/OHGames/Mechanical
+ * 
+ * By O. H. Games
+ * 
+ * Note: some files contain code from other sources so see https://github.com/OHGames/Mechanical/blob/main/USED_CODE_LICENSES.txt for more info.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mehcanical.Aseprite
+{
+    /// <summary>
+    /// The Asprite File class represents an Asprite spritesheet export.
+    /// </summary>
+    /// <remarks>
+    /// This is not to be confused with <c>.ase</c> or <c>.aseprite</c> files.
+    /// </remarks>
+    public class AsepriteFile
+    {
+
+        /// <summary>
+        /// The directory of the file.
+        /// </summary>
+        public string FileDirectory { get; set; }
+
+        /// <summary>
+        /// The name of the file.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The name of the image that this Asprite file is made for.
+        /// </summary>
+        public string Image { get; }
+
+        /// <summary>
+        /// The frames in this file.
+        /// </summary>
+        public List<AspriteFrame> Frames { get; set; } = new List<AspriteFrame>();
+
+        /// <summary>
+        /// The frame tags in this file.
+        /// </summary>
+        public List<AsepriteFrameTag> FrameTags { get; set; } = new List<AsepriteFrameTag>();
+
+        /// <summary>
+        /// The slices in the file.
+        /// </summary>
+        public List<AsepriteSlice> Slices { get; set; } = new List<AsepriteSlice>();
+
+    }
+}
---
+++ Mechanical.Aseprite/AspriteFrame.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * 
+ * This file is part of the Mechanical Game Engine and is licensed under the MIT license.
+ * https://github.com/OHGames/Mechanical
+ * 
+ * By O. H. Games
+ * 
+ * Note: some files contain code from other sources so see https://github.com/OHGames/Mechanical/blob/main/USED_CODE_LICENSES.txt for more info.
+ */
+
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mehcanical.Aseprite
+{
+    /// <summary>
+    /// The Aseprite Frame struct represents a frame from an <see cref="AsepriteFile"/>.
+    /// </summary>
+    public struct AspriteFrame
+    {
+
+        /// <summary>
+        /// The rectangle that represents this frame on the image.
+        /// </summary>
+        public Rectangle FrameRect { get; set; }
+
+        /// <summary>
+        /// The sprite source size. (initial image size)
+        /// </summary>
+        public Rectangle SpriteSourceSize { get; set; }
+
+        /// <summary>
+        /// The frame duration in miliseconds.
+        /// </summary>
+        public int FrameDuration { get; set; }
+
+        /// <summary>
+        /// The frame duration in seconds.
+        /// </summary>
+        public float FrameDurationInSeconds => FrameDuration / 1000;
+
+    }
+}
